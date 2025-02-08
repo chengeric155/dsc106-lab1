db5a9dbbf2569811866d07c6f7f8dfd707caf30f
Eric Cheng <chengeric155@gmail.com>
2025-02-08 00:00:07 -0800

    lab5 final commit

--- projects/projects.js
+++ projects/projects.js
@@ -14,86 +14,10 @@ if (header) {
 
 // pie chart
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-// let arc = arcGenerator({
-//     startAngle: 0,
-//     endAngle: 2 * Math.PI,
-// });
-
-// d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
-
-// let data = [1, 2];
-// let colors = ['gold', 'purple'];
-// let total = 0;
-
-// for (let d of data) {
-//   total += d;
-// }
-
-// let angle = 0;
-// let arcData = [];
-
-// for (let d of data) {
-//   let endAngle = angle + (d / total) * 2 * Math.PI;
-//   arcData.push({ startAngle: angle, endAngle });
-//   angle = endAngle;
-// }
-
-// let arcs = arcData.map((d) => arcGenerator(d));
-
-// let data = [
-//     { value: 1, label: 'apples' },
-//     { value: 2, label: 'oranges' },
-//     { value: 3, label: 'mangos' },
-//     { value: 4, label: 'pears' },
-//     { value: 5, label: 'limes' },
-//     { value: 5, label: 'cherries' },
-// ];
-// let rolledData = d3.rollups(
-//     projects,
-//     (v) => v.length,
-//     (d) => d.year,
-// );
-// let data = rolledData.map(([year, count]) => {
-//     return { value: count, label: year };
-// });
-// let sliceGenerator = d3.pie().value((d) => d.value);
-// let arcData = sliceGenerator(data);
-// let arcs = arcData.map((d) => arcGenerator(d));
-// let colors = d3.scaleOrdinal(d3.schemeTableau10);
-
-// arcs.forEach((arc, idx) => {
-//     d3.select('svg')
-//       .append('path')
-//       .attr('d', arc)
-//       .attr('fill', colors(idx)) // Fill in the attribute for fill color via indexing the colors variable
-// })
-
-// let legend = d3.select('.legend');
-// data.forEach((d, idx) => {
-//     legend.append('li')
-//           .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
-//           .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
-// })
-
-
 
 let query = '';
 
 let searchInput = document.querySelector('.searchBar');
-
-// searchInput.addEventListener('change', (event) => {
-//   // update query value
-//   query = event.target.value;
-//   let filteredProjects = projects.filter((project) => {
-//     let values = Object.values(project).join('\n').toLowerCase();
-//     return values.includes(query.toLowerCase());
-//   });
-
-//   // TODO: render updated projects!
-//   renderProjects(filteredProjects, projectsContainer, 'h2');
-// });
-
-
 
 function renderPieChart(projectsGiven) {
 
