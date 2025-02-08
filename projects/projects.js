6fec465c2c98794a9f85823ae9b6967c85a06ee7
Eric Cheng <chengeric155@gmail.com>
2025-02-07 23:38:22 -0800

    cleaned up a bit

--- projects/projects.js
+++ projects/projects.js
@@ -168,35 +168,3 @@ searchInput.addEventListener('input', (event) => {
     renderProjects(filteredProjects, projectsContainer, 'h2');
     renderPieChart(filteredProjects);
 });
-
-
-
-// let selectedIndex = -1;
-// let svg = d3.select('svg');
-//   svg.selectAll('path').remove();
-//   arcs.forEach((arc, i) => {
-//     svg
-//       .append('path')
-//       .attr('d', arc)
-//       .attr('fill', colors(i))
-//       .on('click', () => {
-//         selectedIndex = selectedIndex === i ? -1 : i;
-        
-//         svg
-//           .selectAll('path')
-//           .attr('class', (_, idx) => (
-//             // TODO: filter idx to find correct pie slice and apply CSS from above
-//           ));
-//         legend
-//           .selectAll('li')
-//           .attr('class', (_, idx) => (
-//             // TODO: filter idx to find correct legend and apply CSS from above
-//           ));
-//         if (selectedIndex === -1) {
-//             renderProjects(projects, projectsContainer, 'h2');
-//         } else {
-//             // TODO: filter projects and project them onto webpage
-//             // Hint: `.label` might be useful
-//         }
-//       });
-// });
