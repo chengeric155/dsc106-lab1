24849f957f22152541c6cb6331e6e6f507a5c139
Eric Cheng <chengeric155@gmail.com>
2025-02-14 17:51:15 -0800

    added main.js

---
+++ meta/main.js
@@ -0,0 +1,94 @@
+let data = [];
+
+async function loadData() {
+    data = await d3.csv('meta/loc.csv', (row) => ({
+        ...row,
+        line: Number(row.line), // or just +row.line
+        depth: Number(row.depth),
+        length: Number(row.length),
+        date: new Date(row.date + 'T00:00' + row.timezone),
+        datetime: new Date(row.datetime),
+    }));
+    displayStats();
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  await loadData();
+});
+
+let commits = d3.groups(data, (d) => d.commit);
+
+
+function processCommits() {
+    commits = d3
+        .groups(data, (d) => d.commit)
+        .map(([commit, lines]) => {
+        let first = lines[0];
+        let { author, date, time, timezone, datetime } = first;
+        let ret = {
+            id: commit,
+            url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+            author,
+            date,
+            time,
+            timezone,
+            datetime,
+            hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+            totalLines: lines.length,
+        };
+
+        Object.defineProperty(ret, 'lines', {
+            value: lines,
+            writable: false, 
+            enumerable: false, 
+            configurable: false,
+        });
+
+        return ret;
+    });
+}
+
+function displayStats() {
+    // Process commits first
+    processCommits();
+  
+    // Create the dl element
+    const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+  
+    // Add total LOC
+    dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+    dl.append('dd').text(data.length);
+  
+    // Add total commits
+    dl.append('dt').text('Total commits');
+    dl.append('dd').text(commits.length);
+  
+    // Add more stats as needed...
+    let avgLineLength = d3.mean(data, (d) => d.length);
+    dl.append('dt').text('Average Line Length');
+    dl.append('dd').text(avgLineLength.toFixed(1));
+
+    dl.append('dt').text('Number of Files');
+    dl.append('dd').text(d3.group(data, d => d.file).size)
+
+    const fileLengths = d3.rollups(
+        data,
+        (v) => d3.max(v, (v) => v.line),
+        (d) => d.file
+    );
+    const averageFileLength = d3.mean(fileLengths, (d) => d[1]);
+    dl.append('dt').text('Average File Length');
+    dl.append('dd').text(averageFileLength);
+
+    const workByPeriod = d3.rollups(
+        data,
+        (v) => v.length,
+        (d) => new Date(d.datetime).toLocaleString('en', { dayPeriod: 'short' })
+    );
+    const maxPeriod = d3.greatest(workByPeriod, (d) => d[1])?.[0];
+    dl.append('dt').text('Time of Day');
+    dl.append('dd').text(maxPeriod);
+}
+
+const width = 1000;
+const height = 600;
